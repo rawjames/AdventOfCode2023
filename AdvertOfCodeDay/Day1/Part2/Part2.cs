c54f788565200488988077dfd5f4fc90b5ac5ebf
rawjames <rawjames@hotmail.com>
2023-12-07 09:23:46 +0000

    Day1

---
+++ AdvertOfCodeDay/Day1/Part2/Part2.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdvertOfCode.Day1.Part1;
+
+public class Day1Part2
+{
+    public void RunMe()
+    {
+        var lines = File.ReadAllLines("day1\\day1Input.txt");
+        var numbers = "[0-9]|zero|one|two|three|four|five|six|seven|eight|nine";
+
+        int runningTotal = 0;
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine($"input: {line}");
+            var collection = Regex.Matches(line, numbers);
+            var first = collection[0].Value;
+            var last = collection[^1].Value;
+
+            Console.WriteLine($"first: {first}, last: {last}");
+
+            int number = ParseNumber(first, last);
+            Console.WriteLine($"parsed number: {number}");
+            runningTotal += number;
+        }
+
+        Console.WriteLine($"running total: {runningTotal}");
+    }
+
+    private static int ParseNumber(string first, string last)
+    {
+
+
+        int number;
+        int.TryParse($"{first}{last}", out number);
+        return number;
+    }
+}
---
+++ AdvertOfCodeDay/Day1/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdvertOfCode.Day1.Part1;
+
+var day1Part1 = new Day1Part1();
+day1Part1.RunMe();
+
+var day1Part2 = new Day1Part2();
+day1Part2.RunMe();
