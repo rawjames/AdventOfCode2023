1a69678df75bfe7b37c910bc4f2c39da7d780869
rawjames <rawjames@hotmail.com>
2023-12-09 15:37:40 +0000

    Day1 Part 2

--- Day1/Part2/Part2.cs
+++ Day1/Part2/Part2.cs
@@ -7,16 +7,16 @@ public class Day1Part2
     public void RunMe()
     {
         var lines = File.ReadAllLines("day1Input.txt");
-        var numbers = "[0-9]|zero|one|two|three|four|five|six|seven|eight|nine";
 
         int runningTotal = 0;
+        var regEx = new Regex("[0-9]|zero|one|two|three|four|five|six|seven|eight|nine");
 
         foreach (string line in lines)
         {
             Console.WriteLine($"input: {line}");
-            var collection = Regex.Matches(line, numbers);
-            var first = collection[0].Value;
-            var last = collection[^1].Value;
+            var match = regEx.Match(line);
+            var first = match.Value;
+            var last = FindLastMatch(regEx, line);
 
             Console.WriteLine($"first: {first}, last: {last}");
 
@@ -26,6 +26,22 @@ public class Day1Part2
         }
 
         Console.WriteLine($"running total: {runningTotal}");
+    }
+
+    private string FindLastMatch(Regex regEx, string line)
+    {
+        string compare = string.Empty;
+        for (int i = line.Length - 1; i >= 0; i--)
+        {
+            compare = line[i] + compare;
+            var match = regEx.Match(compare);
+            if(match.Success)
+            {
+                return match.Value;
+            }
+        }
+
+        return string.Empty;    
     }
 
     private static int ParseNumber(string first, string last)
